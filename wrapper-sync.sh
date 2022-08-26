52f1da83fc52defe3f886e97c090e958a484a0dd
Darek <d4ptak@gmail.com>
2022-08-26 21:59:40 +0200

    Check if file not exists

--- wrapper-sync.sh
+++ wrapper-sync.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-FILE='../names_to_sync.txt'
+FILE='../repositories_to_sync.txt'
 
-while read NAME; do
-   if [ -d "$NAME" ]; then
-     cd $NAME
+if [ ! -f "$FILE" ]; then 
+  echo "File $FILE does not exist!"
+  exit 0
+fi
+
+while read REPO; do
+   if [ -d "$REPO" ]; then
+     cd $REPO
+     if [ ! -f sync.sh ]; then
+	ln -s ../git-sync-script/sync.sh sync.sh
+     fi	
      ./sync.sh
    fi
 done < $FILE
